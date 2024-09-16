416a032a42ffffa1d4704551709503da2880cdb6
Ken Tran <treys@kensington-20.ht.home>
2024-09-16 15:40:44 -0400

    added multiples file

---
+++ src/Multiples.java
@@ -0,0 +1,25 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        int count = 0;
+        while (i < 1000) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                count++;
+            } else if (divisibleBy3) {
+                count++;
+            } else if (divisibleBy5) {
+                count++;
+            }
+
+            i += 1;
+        }
+
+        System.out.println(count);
+    }
+}
