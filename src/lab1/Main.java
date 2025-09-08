59ceda6d133d1242bc55889551c7d2347498a923
treys <kensington.tran@mail.utoronto.ca>
2025-09-08 15:53:25 -0400

    small changes

---
+++ src/lab1/Main.java
@@ -0,0 +1,5 @@
+import lab1.FizzBuzz;
+
+public static void main(String[] args) {
+    FizzBuzz.doFizzBuzz(5);
+}
--- test/lab1/MultiplesTest.java
+++ test/lab1/MultiplesTest.java
@@ -1,6 +1,6 @@
 package lab1;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 /*
   Some test cases for Multiples.
