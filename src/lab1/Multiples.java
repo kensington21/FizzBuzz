3aeefaf2c131690acd5d6b4abed9dc2106b85e31
treys <kensington.tran@mail.utoronto.ca>
2025-09-09 15:32:36 -0400

    correction to multiples final checks

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -7,6 +7,23 @@ public class Multiples {
         System.out.println(multiples());
     }
 
+    static int multiples() {
+        int total = 0;
+
+        for (int i = 1; i < 1000; i++){
+
+            boolean divisibleby3 = i % 3 == 0;
+            boolean divisibleby5 = i % 5 == 0;
+
+            if (divisibleby3){
+                total++;
+            } else if (divisibleby5){
+                total++;
+            }
+        }
+        return total;
+    }
+
     static int multiples(int n, int a, int b) {
         //total integer
         int total = 0;
@@ -14,13 +31,13 @@ public class Multiples {
         for (int i = 1; i < n; i++){
 
             //boolean statements
-            boolean divisibleby3 = i % a == 0;
-            boolean divisibleby5 = i % b == 0;
+            boolean divisiblebya = i % a == 0;
+            boolean divisiblebyb = i % b == 0;
 
             //body of code
-            if (divisibleby3){
+            if (divisiblebya){
                 total++;
-            } else if (divisibleby5){
+            } else if (divisiblebyb){
                 total++;
             }
         };
